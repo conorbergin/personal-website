b8d1a190c0bc70f1e1997e9623a32da40ed3e11e
= <=>
2023-04-26 13:23:39 +0100

    started fixie writeup

---
+++ src/routes/fixie/+page.svelte
@@ -0,0 +1,3 @@
+<p>
+    Sometime in 2020 I snapped the derailleur off my vintage road bike. Instead of just replacing the broken part I convinced myself that it would be cheaper to convert the drivetrain to a single speed fixed gear. This turned out to be woefully incorrect. I did end up with a real appreciation for the simplicity of working on old bikes.
+</p>
