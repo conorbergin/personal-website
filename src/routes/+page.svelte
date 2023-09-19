7fe218fdd922958314c2eb85ac98b29589fd9869
Conor <1299651405@mail.com>
2023-09-20 00:38:45 +0100

    contenteditable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     <title>Conor Bergin's Website</title>
 <!-- </head> -->
 
+<p><a href="/contenteditable">Building a contenteditable Text Editor from Scratch</a></p>
 <p><a href="/eleventy-to-sveltekit">From Eleventy to SvelteKit</a></p>
 <p><a href="/aoc-rust">Advent of Code in Rust</a></p>
 <p><a href="/cad">On CAD</a></p>
